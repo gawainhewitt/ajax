a1ec0b57367e115c818d1f0e8d7ee2136b0bbc72
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2025-01-05 14:48:13 +0000

    setting up on netlify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import * as Tone from "tone";
+
+    const synth = new Tone.Synth().toDestination();
+
+</script> 
+
+
+<h1>Welcome to Ajax</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick="{() => {synth.triggerAttackRelease("C4", "8n")}}">hello</button>
