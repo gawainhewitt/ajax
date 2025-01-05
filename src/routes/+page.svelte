4f7ec785842ffe9f3eaaebdef84de8933cba17c5
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2025-01-05 15:18:24 +0000

    circle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,18 @@
 </script> 
 
 
-<h1>Welcome to Ajax</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick="{() => {synth.triggerAttackRelease("C4", "8n")}}">hello</button>
+<button class="button" onclick="{() => {synth.triggerAttackRelease("C4", "8n")}}"></button>
+
+
+<style>
+
+    .button {
+        background-color: yellow;
+        font-size: 2rem;
+        margin: 2em;
+        padding: 2em;
+        border-radius: 50%;
+    }
+
+</style>
